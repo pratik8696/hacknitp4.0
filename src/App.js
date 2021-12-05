ac60e702cd7fb2ce1a6c29010f3b718d40300285
SudhanshuuRanjan <sudhanshur.ug20.ee@nitp.ac.in>
2021-12-05 20:59:43 +0530

    added basic project layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
+import HomePage from './components/homepage/HomePage';
+import Timer from './components/timer/Timer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/timer">
+            <Timer/>
+          </Route>
+        </Switch>
+    </Router>
     </div>
   );
 }

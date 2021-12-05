c1df4814a76aaeac56c48773b924bf095cbff946
SudhanshuuRanjan <sudhanshur.ug20.ee@nitp.ac.in>
2021-12-05 23:06:46 +0530

    finished hero section

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { NavBar } from '../navbar/NavBar'
 import './Homepage.css'
+import hacknitpold from '../../images/hacknitp-old.svg'
+import gameon from '../../images/game-on.svg'
+import curve1 from '../../images/Curve1.svg'
+import Timer from '../timer/Timer'
+
 const HomePage = () => {
     return (
-        <div>
-         Home Page
+        <div className='body'>
+            <NavBar/>
+            <div className="home-body">
+                <div className="home-left">
+                    <img src={hacknitpold}  className='hack-old'/>
+                    <h3 className="home-date">
+                        November 20,2021 - February 19,2022
+                    </h3>
+                    <p className="home-para">
+                        The hack is only a small part of the competition. The real spirit lies in building your network in the growing tech community and collaborating with some of the finest minds of the nation. Just remember, taking the Hackathons too seriously defeats the purpose. Save the seriousness for pitching to investors. Keeping an open mind will open new doors, help build new relationships and acquire new skills.This is hackNITP.
+                    </p>
+                    <div className="home-btns">
+                        <button className='btn-primary'>Register Now</button>
+                        <button className='btn-secondary'>Join Discord</button>
+                    </div>
+                </div>
+                <div className="home-right">
+                    <img src={gameon} alt="" className='game-on'/>
+                </div>
+
+            </div>
+            <Timer/>
         </div>
     )
 }
--- src/components/timer/Timer.js
+++ src/components/timer/Timer.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import './Timer.css'
 
 const Timer = () => {
     return (
         <div>
-           <h1>Timer</h1> 
+           <h1 class="heading">EVENT STARTS IN</h1>
+           <div className="timercont">
+               <div className="timer-box">
+                   <h1 className="big-time">
+                       02
+                   </h1>
+                   <p className="small-time">
+                       Days
+                   </p>
+               </div>
+               <div className="timer-box">
+                   <h1 className="big-time">
+                       02
+                   </h1>
+                   <p className="small-time">
+                       Days
+                   </p>
+               </div>
+               <div className="timer-box">
+                   <h1 className="big-time">
+                       02
+                   </h1>
+                   <p className="small-time">
+                       Days
+                   </p>
+               </div>
+               <div className="timer-box">
+                   <h1 className="big-time">
+                       02
+                   </h1>
+                   <p className="small-time">
+                       Days
+                   </p>
+               </div>
+           </div>
+
         </div>
     )
 }

f22f49194251d975e96b359eeca19137a8491d30
pratik8696 <mepratikanand@gmail.com>
2022-01-06 17:04:34 +0530

    fixed bugs

--- src/components/sponsors/Sponsors.js
+++ src/components/sponsors/Sponsors.js
@@ -6,58 +6,66 @@ const Sponsors = () => {
     return (
         <div>
             <h1 className="heading"><span className='white'>
-                SPONSORS \ </span> PARTNERS</h1>
+                SPONSORS \ </span> PARTNERS
+            </h1>
 
-                <h2 className="level2-heading platinum">
-                    Platinum Sponsors
+            <h2 className="level2-heading platinum">
+                Platinum Sponsors
+            </h2>
+            <div className="platinum-cont">
+                <SponsorBP />
+                <SponsorBP />
+            </div>
+            <h2 className="level2-heading gold">
+                Gold Sponsors
+            </h2>
+            <div className="platinum-cont">
+                <SponsorBP />
+                <SponsorBP />
+                <SponsorBP />
+            </div>
+            <h2 className="level2-heading silver">
+                Silver Sponsors
+            </h2>
+            <div className="platinum-cont">
+                <SponsorBP />
+                <SponsorBP />
+                <SponsorBP />
+                <SponsorBP />
+                <SponsorBP />
+                <SponsorBP />
+                <SponsorBP />
+                <SponsorBP />
+                <SponsorBP />
+                <SponsorBP />
+                <SponsorBP />
+                <SponsorBP />
+
+            </div>
+            <br></br>
+            <br></br>
+            <h1 className="heading"><span className='white'>
+                Lead </span> Organizer
+            </h1>
+            <h2 className="level2-heading platinum">
+                Platinum Sponsors
+            </h2>
+            <div className="enquire-sponsors-cont" data-aos="fade-up">
+                <h1 className="heading"><span className='white'>Want to Sponsor us?</span></h1>
+                <p className="last-yr-sponsors">
+                    Click here to see our last year sponsors
+                    <i class="fab fa-send"></i>
+                </p>
+                <h2 className="heading">
+                    <span className="white">
+                        Contact us at <a href="mailto:hackslash@nitp.ac.in" className='mail-id'> hackslash@nitp.ac.in</a>
+                    </span>
                 </h2>
-                <div className="platinum-cont">
-                    <SponsorBP/>
-                    <SponsorBP/>
-                </div>
-                <h2 className="level2-heading gold">
-                    Gold Sponsors
-                </h2>
-                <div className="platinum-cont">
-                    <SponsorBP/>
-                    <SponsorBP/>
-                    <SponsorBP/>
-                </div>
-                <h2 className="level2-heading silver">
-                    Silver Sponsors
-                </h2>
-                <div className="platinum-cont">
-                    <SponsorBP/>
-                    <SponsorBP/>
-                    <SponsorBP/>
-                    <SponsorBP/>
-                    <SponsorBP/>
-                    <SponsorBP/>
-                    <SponsorBP/>
-                    <SponsorBP/>
-                    <SponsorBP/>
-                    <SponsorBP/>
-                    <SponsorBP/>
-                    <SponsorBP/>
-                    
+                <div className="sponsor-brochure-btn dnld-sponsor-broc">
+                    <button className='btn-primary'>Download Sponsor Brochure</button>
                 </div>
 
-                <div className="enquire-sponsors-cont" data-aos="fade-up">
-                  <h1 className="heading"><span className='white'>Want to Sponsor us?</span></h1>
-                  <p className="last-yr-sponsors">
-                      Click here to see our last year sponsors 
-                      <i class="fab fa-send"></i>
-                  </p>
-                  <h2 className="heading">
-                      <span className="white">
-                          Contact us at <a href="mailto:hackslash@nitp.ac.in" className='mail-id'> hackslash@nitp.ac.in</a>
-                      </span>
-                  </h2>
-                  <div className="sponsor-brochure-btn dnld-sponsor-broc">
-                      <button className='btn-primary'>Download Sponsor Brochure</button>
-                  </div>
-
-                </div>
+            </div>
 
 
         </div>

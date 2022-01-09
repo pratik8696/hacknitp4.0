4fc2c9d31b694dc73787713bfaebb80345bff7b4
Amrit Sharma <go4amrit460@gmail.com>
2022-01-09 10:30:46 +0530

    css fix & Community Partner

--- src/components/organiser/Organiser.js
+++ src/components/organiser/Organiser.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import './Organiser.css'
-import logo from '../../images/hackslash.svg'
 
-const Organiser = () => {
+
+const Organiser = (props) => {
     return (
         <div className='main-cont'>
             <div className="Organiser-card">  
                 <div className="Organiser-card-image">
-                    <img src={logo} alt="HackSlash" className='card-img'/>
+                    <img src={props.logo} alt="HackSlash" className='card-img'/>
                 </div>
                 <div className="Organiser-card-text">
-                    <h1 className="Organiser-card-heading">Hackslash Club</h1><br></br>
-                    <p className="Organiser-card-para">
-                    Hackslash Developers' Club, NIT Patna is a community intended to provide a room for college students to learn and collaborate on a plethora of projects among themselves as well as with mentors. It is open for every student, ranging from novice developers who are just starting, to advanced developers who want to further enhance their skills.
-                    </p><br/>
+                    <h1 className="Organiser-card-heading">{props.name}</h1><br></br>
+                    <p className="Organiser-card-para">{props.desc}</p>
+                   
+                    <br/>
                     <div className="Organiser-card-social-icons">
-                        <a href='https://www.facebook.com/hackSlash.nitp' target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
-                        <a href='https://www.instagram.com/hackslash.nitp/' target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
-                        <a href='https://www.linkedin.com/company/hackslash' target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a>
+                        <a href={props.facebook} target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
+                        <a href={props.insta} target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+                        <a href={props.linkedin} target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a>
                     </div>
                 </div>
             </div>
